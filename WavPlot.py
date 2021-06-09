5ecdbc9984637723449f9b1955b5fce8e5428118
jay <jay.roplekar@gmail.com>
2021-06-09 17:00:33 -0500

    Changes to be committed: 	new file:   WavPlot.py 	This is to help plot wavfile data graphically for debugging

---
+++ WavPlot.py
@@ -0,0 +1,35 @@
+"""
+ Module to plot wav file data
+ Assumes python3 and scipy  and matplotlib are installed
+ Invoke it as  python3 WavPlot.py example_input1.wav 
+"""
+import sys
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+from scipy.io import wavfile
+import scipy.io
+
+
+for arg in sys.argv[1:]:
+        print("Processing:", arg)
+        wav_fname=arg
+        samplerate, data = wavfile.read(wav_fname)
+        try:
+            print(f"number of channels = {data.shape[1]}")
+            length = data.shape[0] / samplerate
+            print(f"length = {length}s")
+            time = np.linspace(0., length, data.shape[0])
+            for i in range(data.shape[1]):
+                plt.plot(time, data[:, i], label=wav_fname.split(".")[0]+"Channel"+str(i))
+        except Exception as e:
+            print (str(e))       
+
+plt.legend()
+plt.xlabel("Time [s]")
+plt.ylabel("Amplitude")
+plt.show()
+	
+
+
